e947ff15406162ee19ba4eff077fc96c6b7e121f
Varalakshmamma <hrvara321@gmail.com>
2026-02-12 22:12:21 +0530

    Day-8 & 9 Internship Work

--- js/welcome.js
+++ js/welcome.js
@@ -51,6 +51,168 @@ for(var i=0;i<5;i++)
 }
 console.log("loop terminates at",i);
 
+//const 
+const pi=3.142;
+console.log(pi);
+
+//undefined
+var fruitsList=0;
+console.log("variable fruitslist",fruitsList);
+
+//null
+
+var scoredPoints=null;
+console.log("scoredPoints",scoredPoints);
+
+//hoisting in JS
+
+
+console.log(spares);
+var spares=10;
+
+//internal flow of hoisting in js
+
+// var spares;
+// console.log(spares);
+// spares=10;
+
+// data structures
+
+// arrays 
+
+var myList=[1,1.24,true,false,'A',"Hello",null];
+console.log("mylist",myList);
+
+// insertion , deletion ,traversal , searching , sorting , push , pop
+
+//splice();
+//syntax
+//splice (starting index , delet count, new value(s))
+
+var habits=["drawing", "Swimming", "Dancing"];
+habits.splice(1,0,"Cycling","OutDoor Games");
+console.log("after insertion",habits);
+
+//deletion
+
+var myorders=[10,20,30,45];
+myorders.splice(2,1,);
+console.log("after deletion",myorders);
+
+// traversal
+// map();
+
+// callback funcs
+// if a accept or returns a function then its called as a callback function
+// function getOrderrs()
+// {
+//     return function test(){}
+// }
+
+// map() is a callback func
+// using this we can access the elements of an array or compute a new array from it
+
+var myOrderHistory=[10,245,27,89];
+
+myOrderHistory.map( (a)=>{ 
+    console.log("map item",a);
+} );
+
+var computedArray=myOrderHistory.map((i)=>{
+    return i-5;
+} );
+
+console.log("computed Array", computedArray);
+
+//searching
+//filter()
+
+//filter() its a callback func
+
+var canceledList=[1001,1010,1020];
+
+var filteredArray=canceledList.filter((a)=>{
+    return a>1005
+});
+console.log("filtered Array", filteredArray);
+
+//sorting
+
+//sort()
+// its a callback func
+
+var distance=[100,26,4,78,56];
+             [4,26,56,78,100];
+
+var sortedArray=distance.sort((a,b)=>{
+    return a-b;
+
+});
+
+console.log("sorted Array", sortedArray);
+
+//push
+var transHistory=["Tr101","Tr102","Tr103"];
+transHistory.push("Tr104");
+console.log("after push",transHistory);
+transHistory.pop();
+console.log("after pop",transHistory);
+
+// JSON (JavaScript Object Notation);
+
+var myProfile={
+    name:"Vara",
+    role:"Web developer",
+    exp:0,
+    edu:"B.E"
+};  
+console.log("myprofile obj",myProfile);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
